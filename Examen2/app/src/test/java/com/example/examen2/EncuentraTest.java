bcc7fb5ebf5b45ddeb8f9ab9593b00f8987e543a
Daniel Serrano Cañas <51306347+dansecan25@users.noreply.github.com>
2025-02-04 23:23:21 -0600

    [UPDATE] test errado

--- Examen2/app/src/test/java/com/example/examen2/EncuentraTest.java
+++ Examen2/app/src/test/java/com/example/examen2/EncuentraTest.java
@@ -10,7 +10,8 @@ import static org.junit.Assert.*;
 public class EncuentraTest{
    @Test
    public void testEncuentra(){
-    List<Integer> numTest = Arrays.asList(1,2,3,4,5,6,7,8,9,10); assertTrue(Calculadora.Encuentra(numTest, 5));
-     assertFalse(Calculadora.Encuentra(numTest, 2000));
+    List<Integer> numTest = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+    assertTrue(Calculadora.Encuentra(numTest, 5));
+     assertTrue(Calculadora.Encuentra(numTest, 2000));
    }
 }
