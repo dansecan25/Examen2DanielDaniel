22bc4430f356d5c0ee7b557c5bdb193188ebbd2c
dansecan2554 <danse.games254@gmail.com>
2025-02-04 21:59:10 -0600

    [ADD] test Encuentra

---
+++ Examen2/app/src/test/java/com/example/examen2/EncuentraTest.java
@@ -0,0 +1,14 @@
+package com.example.examen2;
+
+import org.junit.Test;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class EncuentraTest{
+   @Test
+   public void testEncuentra(){
+    List<Integer> numTest = Array.asLArray.asList(1,2,3,4,5,6,7,8,9,10); assertTrue(Calculadora.Encuentra(numTest, 5));
+     assertFalse(Calculadora.Encuentra(numTest, 2000)):
+   }
+}
